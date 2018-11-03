9465146d2197b2881996a08f1506cc3095b5fc12
Fabrice <fabrice.hong@gmail.com>
2018-11-03 11:58:31 +0100

    linking react native vector icons

--- workshops/meetup-6-pulse/android/app/src/main/java/com/meetup3maps/MainApplication.java
+++ workshops/meetup-6-pulse/android/app/src/main/java/com/meetup3maps/MainApplication.java
@@ -3,6 +3,8 @@ package com.meetup3maps;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,7 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new MapsPackage()
+            new VectorIconsPackage(),
+            new MapsPackage()
       );
     }
 
