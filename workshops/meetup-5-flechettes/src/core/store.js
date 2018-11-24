47e742f5443479fb945c5bdbdfc123721f04b903
Arnaud Béguin <abeguin@octo.com>
2018-11-24 14:23:18 +0100

    add our navigation reducer to the store

--- workshops/meetup-5-flechettes/src/core/store.js
+++ workshops/meetup-5-flechettes/src/core/store.js
@@ -1,5 +1,16 @@
-import {createStore} from 'redux';
+import {applyMiddleware, combineReducers, createStore} from 'redux';
+import {createReactNavigationReduxMiddleware} from 'react-navigation-redux-helpers';
+import navigationReducer from '../redux/reducers/navigationReducer';
 
-const store = createStore();
+// Combine reducers
+const appReducer = combineReducers({
+  nav: navigationReducer,
+});
+
+// Note: createReactNavigationReduxMiddleware must be run before reduxifyNavigator
+const middleware = createReactNavigationReduxMiddleware('root',
+  state => state.nav);
+
+const store = createStore(appReducer, applyMiddleware(middleware));
 
 export default store;
