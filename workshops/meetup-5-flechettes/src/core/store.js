71831872c328a18f44f4bb217262924bc2bbb8f3
Arnaud Béguin <abeguin@octo.com>
2018-11-25 10:35:04 +0100

    add redux-logger in 'dev' env

--- workshops/meetup-5-flechettes/src/core/store.js
+++ workshops/meetup-5-flechettes/src/core/store.js
@@ -2,17 +2,26 @@ import {applyMiddleware, combineReducers, createStore} from 'redux';
 import {createReactNavigationReduxMiddleware} from 'react-navigation-redux-helpers';
 import navigationReducer from '../redux/reducers/navigationReducer';
 import playersReducer from '../redux/reducers/playersReducer';
+import {createLogger} from 'redux-logger';
 
 // Combine reducers
 const appReducer = combineReducers({
-  nav: navigationReducer,
-  players: playersReducer
+  nav: navigationReducer, players: playersReducer,
 });
+
+const env = process.env['ENV'];
 
 // Note: createReactNavigationReduxMiddleware must be run before reduxifyNavigator
 const middleware = createReactNavigationReduxMiddleware('root',
   state => state.nav);
 
-const store = createStore(appReducer, applyMiddleware(middleware));
+let logger;
+if (env === 'dev') {
+  logger = createLogger({
+    collapsed: true,
+  });
+}
+
+const store = createStore(appReducer, applyMiddleware(middleware, logger));
 
 export default store;
