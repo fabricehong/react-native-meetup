d34ac16aaba31b3334480a8183fc698f11b001b6
Arnaud Béguin <abeguin@octo.com>
2018-11-26 20:53:37 +0100

    list all actions regarding a player

--- workshops/meetup-5-flechettes/src/redux/actions/playersActions.js
+++ workshops/meetup-5-flechettes/src/redux/actions/playersActions.js
@@ -1,7 +1,28 @@
 export const ADD_PLAYER = 'ADD_PLAYER';
+export const REMOVE_PLAYER = 'REMOVE_PLAYER';
+export const SET_PLAYER_NAME = 'SET_PLAYER_NAME';
+export const SET_PLAYER_SCORE = 'SET_PLAYER_SCORE';
 
 export function addPlayer(name) {
   return {
     type: ADD_PLAYER, name: name,
+  };
+}
+
+export function removePlayer(id) {
+  return {
+    type: REMOVE_PLAYER, id: id,
+  };
+}
+
+export function setPlayerName(id, name) {
+  return {
+    type: SET_PLAYER_NAME, id: id,
+  };
+}
+
+export function setPlayerScore(id) {
+  return {
+    type: SET_PLAYER_SCORE, id: id,
   };
 }
--- workshops/meetup-5-flechettes/src/redux/actions/playersActions.test.js
+++ workshops/meetup-5-flechettes/src/redux/actions/playersActions.test.js
@@ -1,5 +1,9 @@
 import mockStore from '../../core/__mocks__/mockStore';
-import {addPlayer} from './playersActions';
+import {
+  addPlayer,
+  removePlayer,
+  setPlayerName, setPlayerScore,
+} from './playersActions';
 
 let store;
 
@@ -11,6 +15,21 @@ describe('playersActions', () => {
 
   test('addPlayer should create an ADD_PLAYER action with the player name', () => {
     store.dispatch(addPlayer('Jules'));
+    expect(store.getActions()).toMatchSnapshot();
+  });
+
+  test('addPlayer should create an ADD_PLAYER action with the player name', () => {
+    store.dispatch(removePlayer(0));
+    expect(store.getActions()).toMatchSnapshot();
+  });
+
+  test('addPlayer should create an ADD_PLAYER action with the player name', () => {
+    store.dispatch(setPlayerName(0, 'Jules'));
+    expect(store.getActions()).toMatchSnapshot();
+  });
+
+  test('addPlayer should create an ADD_PLAYER action with the player name', () => {
+    store.dispatch(setPlayerScore(1, 300));
     expect(store.getActions()).toMatchSnapshot();
   });
 
