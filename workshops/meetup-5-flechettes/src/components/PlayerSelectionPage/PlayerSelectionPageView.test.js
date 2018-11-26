284cd0609911aa5bd54f026537893411d8280386
Arnaud Béguin <abeguin@octo.com>
2018-11-26 20:35:49 +0100

    fix playerselectionpageview test by passing the props needed to render it

--- workshops/meetup-5-flechettes/src/components/PlayerSelectionPage/PlayerSelectionPageView.test.js
+++ workshops/meetup-5-flechettes/src/components/PlayerSelectionPage/PlayerSelectionPageView.test.js
@@ -2,9 +2,15 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import PlayerSelectionPageView from './PlayerSelectionPageView';
 
+const props = {
+  players: []
+};
+
 describe('PlayerSelectionPageView', () => {
+
   test('should renders correctly', () => {
-    const tree = renderer.create(<PlayerSelectionPageView/>).toJSON();
+    const tree = renderer.create(<PlayerSelectionPageView {...props}/>).toJSON();
     expect(tree).toMatchSnapshot();
   });
+
 });
