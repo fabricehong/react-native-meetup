dcaf17d20a6ce0c491cf6037e7686b4ac276ec87
Giodemaio <Giodemaioc@gmail.com>
2018-12-09 21:17:55 +0100

    ajoute des contrôles Input

--- workshops/meetup-5-flechettes/src/PlayerSelection.js
+++ workshops/meetup-5-flechettes/src/PlayerSelection.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, TextInput, Button, Dimensions,StyleSheet } from 'react-native';
+import { Text, View, TextInput, Button, Dimensions } from 'react-native';
 import PrimaryButton from './PrimaryButton';
 
 const { Twidth, Theight } = Dimensions.get('window');
@@ -42,70 +42,48 @@ export default class PlayerSelection extends Component {
             alert('Erreur: Il faut saisir au moins deux joueur.');
         }
     }
-
-    render() {
-    
-        return (
-            <View style={styles.container}>
-                {this.state.players.map((ele) => 
-                    <View>
-                        <Text key={ele+'_2'} style={styles.nomPlayer}>{ele}</Text>
-                        <View key={ele+'_3'} style={styles.boutonRemove}>
-				            <Button key={ele+'_1'}  title="x" onPress={() => this.removePlayer(ele)}/>
-			            </View>  
-                    </View> 
-                )}
-               
-                 <TextInput
-                     style={styles.inputNomPlayer}
-                     onChangeText={(text) => this.setState({ text })}
-                     placeholder='Nom jouer'
-                     value={this.state.text}
-                     
-                 />
-
-                 <View  style={styles.boutonAdd}>
-                     <Button title="+"  onPress={this.addPlayer}  /> 
-                 </View>
-                
-                <View  style={styles.boutonStart} >
+        render() {
+            const {toutSurUneligne, nomPlayer, lesBoutons, bouton } = styles;
+            return (
+                <View>
+                    <Text>Enter your players !</Text>
+                    <TextInput
+                        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                        onChangeText={(text) => this.setState({ text })}
+                        value={this.state.text}
+                    />
+                    <Button title="add" onPress={this.addPlayer}/>  
+                    {this.state.players.map((ele) => 
+                        <View key={ele} style={toutSurUneligne} >
+                            <Text key={ele+'_2'} style={nomPlayer}>Joueur : {ele}</Text>
+                            <View key={ele+'_3'} style={lesBoutons}>
+                    <Button key={ele+'_1'} style={bouton} title="X" onPress={() => this.removePlayer(ele)}/>
+                    <Button key={ele+'_4'} style={bouton} title="O" onPress={() => this.removePlayer(ele)}/>
+                </View>
+                        </View> 
+                    )}
                     <PrimaryButton label="Start" onPress={this.startGame}/>
-                </View> 
-            </View>
-        )
+                </View>
+            )
+        }
     }
-}
-
-const styles = StyleSheet.create({
-	container: {
-        flex: 1,
-        flexDirection: 'row',
-     	borderColor: '#d6d7da',
-	},
-
-   
-	nomPlayer: {
-        flex: .9,
-        fontSize: 20,
-       
-        fontWeight: 'bold',
-	},
-	boutonRemove: {
-        flex: .1,  
-    },
-    inputNomPlayer: {
-        flex: .9,
-        fontSize: 20,
-	},
-    boutonAdd: {
-        flex: .1,
-    },
-    boutonStart: {
-		alignItems:'flex-start', 
-    },
-   
-
-});
-
-
+    
+    const styles = {
+        toutSurUneligne: {
+            flexDirection: 'row',
+            borderBottomWidth: 3,
+            borderColor: '#d6d7da',
+        },
+        nomPlayer: {
+            flex: 2,
+        },
+        lesBoutons: {
+            flex: 1,
+            flexDirection: 'row',
+        },
+        bouton: {
+            width : 20,
+            paddingHorizontal: 25, // ne fonctionne pas.		
+        },
+    }
 
