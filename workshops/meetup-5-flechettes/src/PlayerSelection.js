f0d28769df288a475e9046206f667e3d6ed74ac0
Giuseppe De Maio <giodemaioc@gmail.com>
2018-12-09 22:39:59 +0100

    Ecran de saisie des joueurs
    * Reordination composants
    * Controles input field
    * refactoring
    * ajoute des contrôles Input
    * Simplification des contrôles

--- workshops/meetup-5-flechettes/src/PlayerSelection.js
+++ workshops/meetup-5-flechettes/src/PlayerSelection.js
@@ -7,19 +7,27 @@ const { Twidth, Theight } = Dimensions.get('window');
 export default class PlayerSelection extends Component {
     constructor(props) {
         super(props);
-        this.state = { text: 'Player 1', players: ['player'] };
+        this.state = { text: '', players: [] };
     }
 
-    editPlayer = (text) => {
-        let players = [ ...this.state.markers ];
-        markers[index] = {...markers[index], key: value};
-        this.setState({ markers });
-    }
+
 
     addPlayer = () => {
+        
         let input = this.state.text.trim();
-        if (input && !this.state.players.includes(input)) {
-            this.setState({ players :  [...this.state.players, input], text: ''});
+       
+        if (input) {
+            if(!this.state.players.includes(input)) {
+                if(this.state.players.length <=10){
+                    this.setState({ players :  [...this.state.players, input], text: ''});
+                }else{
+                    alert('Erreur: Max 10 joueurs');
+                }
+            }else{
+                alert('Erreur: Le nom du joueur est déjà presente dans la liste.');
+            }
+        }else{
+            alert('Erreur: Le nom du joueur ne peux pas être vide.');
         }
     }
 
@@ -28,53 +36,65 @@ export default class PlayerSelection extends Component {
     }
 
     startGame = () => {
-
+        if(this.state.players.length >1){
+            alert('Start Game');
+        }else{
+            alert('Erreur: Il faut saisir au moins deux joueur.');
+        }
     }
-
-    render() {
-    	const {toutSurUneligne, nomPlayer, lesBoutons, bouton } = styles;
-        return (
-            <View>
-                <Text>Enter your players !</Text>
-                <TextInput
-                    style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-                    onChangeText={(text) => this.setState({ text })}
-                    value={this.state.text}
-                />
-                <Button title="add" onPress={this.addPlayer}/>  
-                {this.state.players.map((ele) => 
-                    <View key={ele} style={toutSurUneligne} >
-                        <Text key={ele+'_2'} style={nomPlayer}>Joueur : {ele}</Text>
-                        <View key={ele+'_3'} style={lesBoutons}>
-				<Button key={ele+'_1'} style={bouton} title="X" onPress={() => this.removePlayer(ele)}/>
-				<Button key={ele+'_4'} style={bouton} title="O" onPress={() => this.removePlayer(ele)}/>
-			</View>
-                    </View> 
-                )}
-                <PrimaryButton label="Start" onPress={this.startGame}/>
-            </View>
-        )
+        render() {
+            const {toutSurUneligne, nomPlayer, lesBoutons,inputNom } = styles;
+            return (
+                    <View>
+                        {this.state.players.map((ele) => 
+                        <View key={ele} style={toutSurUneligne} >
+                            <Text key={ele+'_2'} style={nomPlayer}>{ele}</Text>
+                            <View key={ele+'_3'} style={lesBoutons}>
+                                 <Button key={ele+'_1'}  title="X" onPress={() => this.removePlayer(ele)}/>
+                            </View>
+                        </View> 
+                    )}
+                    <View  style={toutSurUneligne} >
+                        <TextInput
+                            style={inputNom}
+                            placeholder='Nom jouer'
+                            onChangeText={(text) => this.setState({ text })}
+                            value={this.state.text}
+                        />
+                        <View style={lesBoutons}>
+                            <Button title="+"  onPress={this.addPlayer}/>  
+                        </View>  
+                    </View>
+                    
+                 
+                    <PrimaryButton label="Start" onPress={this.startGame}/>
+                </View>
+            )
+        }
     }
-}
-
-const styles = {
-	toutSurUneligne: {
-		flexDirection: 'row',
-		borderBottomWidth: 3,
-		borderColor: '#d6d7da',
-	},
-	nomPlayer: {
-		flex: 2,
-	},
-	lesBoutons: {
-		flex: 1,
-		flexDirection: 'row',
-	},
-	bouton: {
-		width : 20,
-		paddingHorizontal: 25, // ne fonctionne pas.		
-	},
-}
-
-
+    
+    const styles = {
+        toutSurUneligne: {
+            flexDirection: 'row',
+            borderBottomWidth: 2,
+            borderColor: '#d6d7da',
+        },
+        nomPlayer: {
+            flex: 10,
+            fontWeight: 'bold',
+            margin: 4,
+        },
+        lesBoutons: {
+            flex: 1,
+            flexDirection: 'row',
+        },
+       
+        inputNom: {
+            flex: 10,
+            color:'grey',
+            margin: 4,
+            borderColor: 'gray', 
+            borderWidth: 1
+        },
+    }
 
