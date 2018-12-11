359523933799dc07feda3f67d807622e327a9383
Alvaro Costa <alvaro.costa@hotmail.fr>
2018-12-11 20:57:15 +0100

    Ecran score de partie

--- workshops/meetup-5-flechettes/src/components/GameScorePage/GameScorePageView.js
+++ workshops/meetup-5-flechettes/src/components/GameScorePage/GameScorePageView.js
@@ -1,10 +1,61 @@
-import React, {PureComponent} from 'react';
-import {View} from 'react-native';
+import React, { PureComponent } from 'react';
+import { View } from 'react-native';
+import {
+  Button,
+  Container,
+  Content,
+  Footer,
+  Form,
+  Icon,
+  Input,
+  Item,
+  Label,
+  Left,
+  List,
+  ListItem,
+  Right,
+  Body,
+  H1
+} from 'native-base';
 
 export class GameScorePageView extends PureComponent {
+  renderResult() {
+    playersList = [
+      {
+        name: 'Fabrice',
+        score: 300
+      },
+      {
+        name: 'Alpha',
+        score: 299
+      },
+      {
+        name: 'Arnaud',
+        score: 12
+      }
+    ];
+
+    const labels = playersList.map((player, index) => {
+      const text = `${player.name}: ${player.score}`;
+      return index === 0 ? <H1 key={index}>{text}</H1> : <Label key={index}>{text}</Label>
+    });
+
+    return (
+      <View style={{ alignItems: 'flex-end' }}>
+        {labels}
+      </View>
+    )
+  }
+
   render() {
     return (
-      <View/>
+      <Container>
+        <Content contentContainerStyle={{ flex: 1 }}>
+          <Body style={{ justifyContent: 'center' }}>
+            {this.renderResult()}
+          </Body>
+        </Content>
+      </Container>
     );
   }
 }
